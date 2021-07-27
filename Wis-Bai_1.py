827ab7e57ad5c797b6cd664af046895d6f9e7407
Trí Phạm <minhtri2805@gmail.com>
2021-07-27 02:15:30 -0500

    Update Wis-Bai_1.py

--- Wis-Bai_1.py
+++ Wis-Bai_1.py
@@ -1,4 +1,4 @@
-# Bai_1
+# Bai_1: Yêu cầu người dùng cung cấp một chuỗi và cho biết đó có phải một palindrome không (palindrome là một chuỗi có thể được viết xuôi hay viết ngược vẫn chỉ cho ra chính nó).
 string = input("Input your string: ")
 if string == string[::-1]:
     print("This is palindrome string")

50d5fa94463906ca3bd38a95cd8016eff875ee9b
Trí Phạm <minhtri2805@gmail.com>
2021-07-27 02:24:16 -0500

    Update Wis-Bai_3.py

--- Wis-Bai_3.py
+++ Wis-Bai_3.py
@@ -1,4 +1,4 @@
-# Bai_3
+# Bai_3: Yêu cầu người dùng nhập một số và xác định xem đó có phải là số nguyên tố hay không.
 n = int(input("Enter your prime number: "))
 if n>1:
     for i in range(2, n):
